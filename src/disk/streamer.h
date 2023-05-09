c2cc47e62df6d51d6aa92d993f10219420ffe664
zmy1213 <1917145106@qq.com>
2023-05-09 04:57:15 -0700

    23

---
+++ src/disk/streamer.h
@@ -0,0 +1,14 @@
+#ifndef STREAMER_H
+#define STREAMER_H
+#include "memory/heap/kheap.h"
+#include "disk.h"
+#include "config.h"
+struct disk_stream
+{
+    int pos;
+    struct disk*disk;
+};
+struct disk_stream* diskstreamer_new(int disk_id);
+int diskstreamer_seek(struct disk_stream* stream,int pos);
+int diskstreamer_read(struct disk_stream*stream,void*out,int total);
+#endif
